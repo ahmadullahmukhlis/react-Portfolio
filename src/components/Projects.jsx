cb14b8a580a73ce801d39b2aebf2e8134c9b26b5
unknown <nasarimukhlis@gmail.com>
2024-01-11 08:06:35 -0800

    the project compnent added

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+      {/* Add your project details here */}
+    </section>
+  );
+};
+
+export default Projects;
