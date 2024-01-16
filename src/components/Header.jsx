c926f1e94c75358d94fe4a81ffd54e6969aaa648
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-01-16 09:30:21 -0800

    the geader is set

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,76 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Ahmad Ullah Mukhlis</h1>
-      <p>Web Developer</p>
-    </header>
-  );
-};
+    <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+
+  <div className="container-fluid">
+
+    {/* Brand on the left */}
+    <a className="navbar-brand" href="#">Ahmad Ullah Mukhlis</a>
+
+    {/* Toggler button for small screens */}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbar"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars"></i>
+    </button>
+
+    {/* Navbar links */}
+    <div className="collapse navbar-collapse justify-content-end" id="navbar">
+
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+              <a className="dropdown-item" href="#">Action</a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">Another action</a>
+            </li>
+            <li><hr className="dropdown-divider" /></li>
+            <li>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+
+</nav>
+
+  )
+}
 
 export default Header;
